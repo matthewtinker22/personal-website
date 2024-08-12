f387896d6983d22cbe083cfa7b5ed48899e3139d
Matthew <mrrt@bellsouth.net>
2024-08-11 21:12:24 -0400

    *Modified comments

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,6 +2,14 @@ export const Header = () => {
   return (
     <div>
       This is where the header goes. This will control app navigation.
+      {/*  Figure out what all pages I need
+          Landing Page
+          About Me
+          Work History/ Projects Worked On
+          Skills
+          Fun Facts? Something fun
+          Contact Info
+          */}
     </div>
   )
 }
