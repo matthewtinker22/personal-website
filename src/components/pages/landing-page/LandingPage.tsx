220f0e97c6ea428d00a323ec5e8868187c451027
Matthew <mrrt@bellsouth.net>
2024-08-11 21:09:03 -0400

    *Modified comments

--- src/components/pages/landing-page/LandingPage.tsx
+++ src/components/pages/landing-page/LandingPage.tsx
@@ -2,14 +2,6 @@ export const LandingPage = () => {
   return (
     <div>
       This is the landing page
-      {/*  TODO: Figure out what all pages I need
-          Landing Page
-          About Me
-          Work History/ Projects Worked On
-          Skills
-          Fun Facts? Something fun
-          Contact Info
-          */}
     </div>
   )
 }
